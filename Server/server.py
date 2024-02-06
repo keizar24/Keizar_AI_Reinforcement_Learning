36014a50089d52268f8412d881d414e4318dfb17
Fengkai Liu <fl1121@ic.ac.uk>
2024-02-06 18:35:00 +0000

    Parse data from Kotlin AI.

--- Server/server.py
+++ Server/server.py
@@ -1,8 +1,10 @@
+import numpy as np
 from flask import Flask, request, jsonify
 import requests
 import json
 
 from AI.GameAI import GameAI
+from AI.connectServer import get_board, parseStr
 
 app = Flask(__name__)
 
@@ -25,8 +27,8 @@ def request_move(player):
     # get the move from the kotlin server
 
     # set up the url
-    url = f'http://127.0.0.1:49152/moves/{player.lower()}' \
-        if type == 'train' else f'http://127.0.0.1:49152/moves/{player.lower()}'  # TODO: new url for testing
+    url = f'http://localhost:49152/moves/{player.lower()}' \
+        if type == 'train' else f'http://localhost:49152/moves/{player.lower()}'  # TODO: new url for testing
 
     # return f'got data, {state}'
 
@@ -34,6 +36,21 @@ def request_move(player):
 
     response = session.post(url, data=json_data, headers={'Content-Type': 'application/json'})
     return response.text
+
+
+def refactor_board(white_list, black_list):
+    cur_board = get_board()
+    for i in range(8):
+        for j in range(8):
+            if cur_board[i, j] == 0:
+                cur_board[i, j] = 6
+            cur_board[i, j] %= 10
+    for [x, y] in white_list:
+        cur_board[x, y] += 10
+    for [x, y] in black_list:
+        cur_board[x, y] += 20
+    print(cur_board)
+    return np.array(cur_board)
 
 
 @app.route('/AI/<player>', methods=['GET', 'POST'])
@@ -49,7 +66,12 @@ def response_best_move(player):
     white_pieces = data.get('white_pieces')
     black_pieces = data.get('black_pieces')
     print(black_pieces, white_pieces, moves)
-
+    state = refactor_board(white_pieces, black_pieces)
+    actions = []
+    for (fx, fy, tx, ty, isCapture) in moves:
+        isCapture = 1 if isCapture else 0
+        actions.append((fx, fy, tx, ty, isCapture))
+    print(actions)
     # pieces: [6,1]
     # move: [1,2,1,3,True]
 
